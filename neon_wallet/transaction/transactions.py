4e4e96856dfd7174d42c3f463e34a87b619867ab
kenzokaitoshi <kenzo.kaito@yahoo.com>
2023-05-13 10:15:00 +0200

    put all comment in english, fix some pylint warning

--- neon_wallet/transaction/transactions.py
+++ neon_wallet/transaction/transactions.py
@@ -1,52 +1,57 @@
-# Définir une fonction qui renvoie l'identifiant de transaction
-# d'une transaction
+"""module transactions"""
+# Define a function that returns the transaction id
+# of a transaction
 import hashlib
 from typing import Any, List
 
-# Importer le module ecdsa pour la signature et la vérification ECDSA
+# Import ecdsa module for ECDSA signing and verification
 import ecdsa
 from neon_wallet.transaction.transaction import Transaction
 from neon_wallet.transaction.tx_in import TxIn
 from neon_wallet.transaction.tx_out import TxOut
 from neon_wallet.transaction.unspent_tx_out import UnspentTxOut
 
+COINBASE_AMOUNT: float = 50
+
 
 def get_transaction_id(transaction: Transaction) -> str:
     """get transaction id"""
-    # Extraire la liste des entrées de transaction de la transaction
+    # Extract list of transaction entries from transaction
     tx_ins = transaction.txIns
-    # Concaténer les identifiants et les indices des sorties de
-    # transaction des entrées de transaction
-    tx_in_content = "".join([tx_in.txOutId + str(tx_in.txOutIndex) for tx_in in tx_ins])
-    # Extraire la liste des sorties de transaction de la transaction
+    # Concatenate the identifiers and indices of the outputs of
+    # transaction of transaction entries
+    tx_ = [tx_in.txOutId + str(tx_in.txOutIndex) for tx_in in tx_ins]
+    tx_in_content = "".join(tx_)
+    # Extract list of transaction outputs from transaction
     tx_outs = transaction.txOuts
-    # Concaténer les adresses et les montants des sorties de transaction
+    # Concatenate addresses and amounts from transaction outputs
     tx_out_content = "".join(
         [tx_out.address + str(tx_out.amount) for tx_out in tx_outs]
     )
 
-    # Hacher le contenu des entrées et des sorties de transaction
-    # avec SHA256 et le convertir en chaîne hexadécimale
+    # Hash content of transaction inputs and outputs
+    # with SHA256 and convert it to hex string
     tx_in_out = tx_in_content + tx_out_content
     return hashlib.sha256((tx_in_out).encode()).hexdigest()
 
 
-# Définir une fonction qui valide une transaction en fonction des sorties de transaction non dépensées
+# Define a function that commits a transaction based on unspent
+# transaction outputs
 def validate_transaction(
     transaction: Transaction, a_unspent_tx_outs: List[UnspentTxOut]
 ) -> bool:
     "validate transaction"
-    # Vérifier si la structure de la transaction est valide
+    # Check if transaction structure is valid
     if not is_valid_transaction_structure(transaction):
         return False
 
-    # Vérifier si l'identifiant de la transaction correspond
-    # au hachage du contenu de la transaction
+    # Check if transaction id matches
+    # to the hash of the transaction contents
     if get_transaction_id(transaction) != transaction.id:
         print("invalid tx id: " + transaction.id)
         return False
 
-    # Vérifier si toutes les entrées de transaction sont valides
+    # Check if all transaction inputs are valid
     has_valid_tx_ins = all(
         [
             validate_tx_in(txIn, transaction, a_unspent_tx_outs)
@@ -58,31 +63,32 @@ def validate_transaction(
         print("some of the txIns are invalid in tx: " + transaction.id)
         return False
 
-    # Calculer la somme des montants des entrées de transaction
-    total_tx_in_values = sum(
-        [get_tx_in_amount(txIn, a_unspent_tx_outs) for txIn in transaction.txIns]
-    )
+    # Calculate sum of amounts of transaction inputs
+    t_tx = [get_tx_in_amount(txIn, a_unspent_tx_outs) for txIn in transaction.txIns]
+    total_tx_in_values = sum(t_tx)
 
-    # Calculer la somme des montants des sorties de transaction
+    # Calculate sum of transaction output amounts
     total_tx_out_values = sum([txOut.amount for txOut in transaction.txOuts])
 
-    # Vérifier si les sommes sont égales
+    # Check if sums are equal
     if total_tx_out_values != total_tx_in_values:
-        print(f"totalTxOutValues !== totalTxInValues in tx: {transaction.id}")
+        _id = transaction.id
+        print(f"totalTxOutValues !== totalTxInValues in tx: {_id}")
         return False
 
     return True
 
 
-# Définir une fonction qui valide une entrée de transaction en fonction des sorties de transaction non dépensées
+# Define a function that validates a transaction input against
+# unspent transaction outputs
 def validate_tx_in(
     tx_in: TxIn,
     transaction: Transaction,
     a_unspent_tx_outs: List[UnspentTxOut],
 ) -> bool:
     """validate transaction in"""
-    # Trouver la sortie de transaction non dépensée qui
-    # correspond à l'entrée de transaction
+    # Find the unspent transaction output that
+    # matches transaction input
     referenced_uTx_out = next(
         (
             uTxO
@@ -95,49 +101,55 @@ def validate_tx_in(
         print("referenced txOut not found: " + str(tx_in))
         return False
 
-    # Extraire l'adresse de la sortie de transaction non dépensée
+    # Extract address from unspent transaction output
     address = referenced_uTx_out.address
 
-    # Créer une clé publique ECDSA à partir de l'adresse
-    key = ecdsa.VerifyingKey.from_string(bytes.fromhex(address), curve=ecdsa.SECP256k1)
+    # Create an ECDSA public key from the address
+    key = ecdsa.VerifyingKey.from_string(
+        bytes.fromhex(address),
+        curve=ecdsa.SECP256k1,
+    )
 
-    # Vérifier la signature de l'entrée de transaction avec la clé publique et l'identifiant de la transaction
+    # Check the signature of the transaction entry with the
+    # public key and transaction id
     valid_signature = key.verify(
         bytes.fromhex(tx_in.signature), bytes.fromhex(transaction.id)
     )
     if not valid_signature:
-        print(
-            f"invalid txIn signature: %s txId: %s address: %s {tx_in.signature}, {transaction.id}, {referenced_uTx_out.address}"
-        )
+        tx_s = tx_in.signature
+        tx_id = transaction.id
+        ref = referenced_uTx_out.address
+        print(f"invalid txIn signature: {tx_s} txId: {tx_id} address: {ref}")
         return False
 
     return True
 
 
-# Définir une fonction qui vérifie si la structure d'une transaction est valide
+# Define a function that checks if the structure of a transaction is valid
 def is_valid_transaction_structure(transaction: Transaction) -> bool:
     """check if is valid transaction structure"""
-    # Vérifier si l'identifiant de la transaction est une chaîne de caractères
+    # Check if transaction id is a string
     if not isinstance(transaction.id, str):
         print("transactionId missing")
         return False
 
-    # Vérifier si les entrées de transaction sont une liste
+    # Check if transaction inputs are a list
     if not isinstance(transaction.txIns, list):
         print("invalid txIns type in transaction")
         return False
 
-    # Vérifier si toutes les entrées de transaction ont une structure valide
+    # Check if all transaction inputs have a valid structure
     if not all([is_valid_tx_in_structure(txIn) for txIn in transaction.txIns]):
         return False
 
-    # Vérifier si les sorties de transaction sont une liste
+    # Check if transaction outputs are a list
     if not isinstance(transaction.txOuts, list):
         print("invalid txOuts type in transaction")
         return False
 
-    # Vérifier si toutes les sorties de transaction ont une structure valide
-    if not all([is_valid_tx_out_structure(txOut) for txOut in transaction.txOuts]):
+    # Check if all transaction outputs have a valid structure
+    _tx = [is_valid_tx_out_structure(txOut) for txOut in transaction.txOuts]
+    if not all(_tx):
         return False
 
     return True
@@ -179,41 +191,45 @@ def is_valid_tx_out_structure(tx_out: TxOut) -> bool:
         return True
 
 
-# Définir une fonction qui vérifie si une adresse est valide
+# Define a function that checks if an address is valid
 def is_valid_address(address: str) -> bool:
     """is valid address"""
-    # Vérifier si la longueur de l'adresse est de 130 caractères
+    # Check if address length is 130 characters
     if len(address) != 130:
         print(address)
         print("invalid public key length")
         return False
-    # Vérifier si l'adresse ne contient que des caractères hexadécimaux
+    # Check if the address contains only hexadecimal characters
     elif not address.isalnum():
         print("public key must contain only hex characters")
         return False
-    # Vérifier si l'adresse commence par 04
+    # Check if the address starts with 04
     elif not address.startswith("04"):
         print("public key must start with 04")
         return False
     return True
 
 
-# Définir une fonction qui renvoie le montant d'une
-# entrée de transaction en fonction des sorties de
-# transaction non dépensées
+# Define a function that returns the amount of a
+# transaction input based on the outputs of
+# unspent transaction
 def get_tx_in_amount(
     tx_in: TxIn,
     a_unspent_tx_outs: List[UnspentTxOut],
 ) -> float:
     """get transaction Amount in"""
     return float(
-        find_unspent_tx_out(tx_in.txOutId, tx_in.txOutIndex, a_unspent_tx_outs).amount
+        find_unspent_tx_out(
+            tx_in.txOutId,
+            tx_in.txOutIndex,
+            a_unspent_tx_outs,
+        ).amount
     )
 
 
-# Définir une fonction qui trouve la sortie de transaction
-#  non dépensée qui correspond à un identifiant et un indice
-# de transaction
+# Define a function that finds transaction output
+# unspent which corresponds to an identifier and an index
+# transaction
 def find_unspent_tx_out(
     transaction_id: str, index: int, a_unspent_tx_outs: List[UnspentTxOut]
 ) -> Any:
