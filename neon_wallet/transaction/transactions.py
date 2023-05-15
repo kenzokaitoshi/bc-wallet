8f8bc1081cf18e5a5325f21516b242c4ad109876
kenzokaitoshi <kenzo.kaito@yahoo.com>
2023-05-15 12:52:29 +0200

    add some transaction functions

--- neon_wallet/transaction/transactions.py
+++ neon_wallet/transaction/transactions.py
@@ -4,6 +4,9 @@
 import hashlib
 from typing import Any, List
 
+# Import the collections module to count the occurrences of elements
+import collections
+
 # Import ecdsa module for ECDSA signing and verification
 import ecdsa
 from neon_wallet.transaction.transaction import Transaction
@@ -64,8 +67,9 @@ def validate_transaction(
         return False
 
     # Calculate sum of amounts of transaction inputs
-    t_tx = [get_tx_in_amount(txIn, a_unspent_tx_outs) for txIn in transaction.tx_ins]
-    total_tx_in_values = sum(t_tx)
+    total_tx_in_values = sum(
+        [get_tx_in_amount(txIn, a_unspent_tx_outs) for txIn in transaction.tx_ins]
+    )
 
     # Calculate sum of transaction output amounts
     total_tx_out_values = sum([txOut.amount for txOut in transaction.tx_outs])
@@ -104,7 +108,7 @@ def validate_tx_in(
 
     # Extract address from unspent transaction output
     address = referenced_uTx_out.address
-    print('ref address', address)
+    print("ref address", address)
 
     # Create an ECDSA public key from the address
     key = ecdsa.VerifyingKey.from_string(
@@ -244,3 +248,143 @@ def find_unspent_tx_out(
         ),
         None,
     )
+
+
+# Define a function to check if a list of txIns has duplicates
+def has_duplicates(tx_ins: List[TxIn]) -> Any:
+    """check if txIns list has duplicates"""
+    # Create a dictionary that counts the number of times
+    # a txIn appears in the list
+    groups = collections.Counter(
+        txIn.tx_out_id + str(txIn.tx_out_index) for txIn in tx_ins
+    )
+    # Iterate through the dictionary and show txIns that
+    # appear more than once
+    for key, value in groups.items():
+        if value > 1:
+            print("duplicate txIn: " + key)
+            return True
+    # If no txIn appears more than once, return False
+    return False
+
+
+# Définir une fonction pour convertir un nombre en hexadécimal
+def to_hex_string(byte_array: List[Any]) -> str:
+    """convert to hex"""
+    # Définir une fonction pour convertir un tableau d'octets en
+    # une chaîne hexadécimale
+    # Créer une liste vide pour stocker les caractères hexadécimaux
+    hex_list = []
+    # Parcourir le tableau d'octets
+    for byte in byte_array:
+        # Appliquer un masque binaire pour ne garder que les
+        # 8 bits de poids faible
+        byte = byte & 0xFF
+        # Convertir l'octet en hexadécimal et ajouter un zéro
+        # devant si nécessaire
+        hex_char = hex(byte)[2:].zfill(2)
+        # Ajouter le caractère hexadécimal à la liste
+        hex_list.append(hex_char)
+    # Joindre les caractères hexadécimaux en une seule chaîne
+    # et la retourner
+    return "".join(hex_list)
+
+
+# Définir une fonction pour signer un txIn avec une clé
+# privée et une liste de UnspentTxOuts
+def sign_tx_in(
+    transaction: Transaction,
+    tx_in_index: int,
+    private_key: str,
+    a_unspent_tx_outs: List[UnspentTxOut],
+) -> Any:
+    """sign tx in"""
+    # Récupérer le txIn à signer
+    tx_in = transaction.tx_ins[tx_in_index]
+
+    # Récupérer l'id de la transaction à signer
+    data_to_sign = transaction.id
+
+    # Trouver le UnspentTxOut qui correspond au txIn
+    ref_unspent_tx_out = find_unspent_tx_out(
+        tx_in.tx_out_id, tx_in.tx_out_index, a_unspent_tx_outs
+    )
+
+    # Si le UnspentTxOut n'existe pas, lever une exception
+    if ref_unspent_tx_out is None:
+        print("could not find referenced txOut")
+        raise ValueError("could not find referenced txOut")
+
+    # Récupérer l'adresse du UnspentTxOut
+    referenced_address = ref_unspent_tx_out.address
+
+    # Vérifier que la clé publique dérivée de la clé privée
+    # correspond à l'adresse du UnspentTxOut
+    if get_public_key(private_key) != referenced_address:
+        print(
+            "trying to sign an input with private"
+            + " key that does not match the address that is referenced in txIn"
+        )
+        raise ValueError()
+
+    # Créer une clé ecdsa à partir de la clé privée en hexadécimal
+    key = ecdsa.SigningKey.from_string(
+        bytes.fromhex(private_key), curve=ecdsa.SECP256k1
+    )
+
+    # Signer les données avec la clé et retourner la signature
+    # en hexadécimal
+    signature = to_hex_string(key.sign(data_to_sign))
+    return signature
+
+
+# Définir une fonction pour obtenir la clé publique à partir
+# de la clé privée
+def get_public_key(a_private_key: str) -> Any:
+    """get public key"""
+    # Créer une clé ecdsa à partir de la clé privée en hexadécimal
+    key = ecdsa.SigningKey.from_string(
+        bytes.fromhex(a_private_key), curve=ecdsa.SECP256k1
+    )
+    # Récupérer la clé publique associée et la retourner en hexadécimal
+    return key.get_verifying_key().to_string().hex()
+
+
+# Définir une fonction pour mettre à jour la liste des UnspentTxOuts
+# à partir d'une liste de transactions
+def update_unspent_tx_outs(
+    a_transactions: List[Transaction],
+    a_unspent_tx_outs: List[UnspentTxOut],
+) -> List[UnspentTxOut]:
+    """update unspent tx outs"""
+    # Créer une nouvelle liste de UnspentTxOuts à partir des txOuts
+    # des transactions
+    new_unspent_tx_outs = []
+    for _t in a_transactions:
+        for index, tx_out in enumerate(_t.tx_outs):
+            new_unspent_tx_outs.append(
+                UnspentTxOut(_t.id, index, tx_out.address, tx_out.amount)
+            )
+
+    # Créer une liste de UnspentTxOuts consommés à partir des txIns
+    # des transactions
+    consumed_tx_outs = []
+    for _t in a_transactions:
+        for tx_in in _t.tx_ins:
+            consumed_tx_outs.append(
+                UnspentTxOut(tx_in.tx_out_id, tx_in.tx_out_index, "", 0)
+            )
+
+    # Filtrer la liste des UnspentTxOuts existants en enlevant
+    # ceux qui sont consommés
+    resulting_unspent_tx_outs = []
+    for u_txo in a_unspent_tx_outs:
+        if not find_unspent_tx_out(
+            u_txo.tx_out_id, u_txo.tx_out_index, consumed_tx_outs
+        ):
+            resulting_unspent_tx_outs.append(u_txo)
+
+    # Ajouter les nouveaux UnspentTxOuts à la liste résultante
+    resulting_unspent_tx_outs.extend(new_unspent_tx_outs)
+
+    return resulting_unspent_tx_outs
