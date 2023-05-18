74fca4db88922a72f77584fa0d1f3b90c50f8e14
kenzokaitoshi <kenzo.kaito@yahoo.com>
2023-05-18 14:55:55 +0200

    add update_transaction_pool and has_tx_in funct

--- neon_wallet/transaction_pool/transaction_pool.py
+++ neon_wallet/transaction_pool/transaction_pool.py
@@ -1,6 +1,6 @@
-"""transaction pool module it will make it possible to manage a set of 
-    transactions waiting to be included in a node of the transaction network. 
-    Each node in the network has its own transaction pool, which can vary 
+"""transaction pool module it will make it possible to manage a set of
+    transactions waiting to be included in a node of the transaction network.
+    Each node in the network has its own transaction pool, which can vary
     depending on the node's rules and settings."""
 
 import copy
@@ -79,3 +79,47 @@ def get_tx_pool_ins(a_transaction_pool: List[Transaction]) -> List[TxIn]:
     # Use list comprehension to extract entries Transaction
     # of each transaction
     return [txIn for tx in a_transaction_pool for txIn in tx.tx_ins]
+
+
+# Define a function to check if a transaction input
+# exists in a list of unspent outputs
+def has_tx_in(tx_in: TxIn, unspent_tx_outs: List[UnspentTxOut]) -> bool:
+    """has tx in"""
+    # Use filter function to find unspent output
+    # corresponding to transaction input
+    found_tx_in = next(
+        filter(
+            lambda uTxO: uTxO.tx_out_id == tx_in.tx_out_id
+            and uTxO.tx_out_index == tx_in.tx_out_index,
+            unspent_tx_outs,
+        ),
+        None,
+    )
+    # Return True if unspent output exists, False otherwise
+    return found_tx_in is not None
+
+
+# Define a function to update the transaction pool
+# with unspent outputs
+def update_transaction_pool(unspent_tx_outs: List[UnspentTxOut]) -> None:
+    """update transactionPool"""
+    # Create an empty list to store invalid transactions
+    invalid_txs = []
+    # Browse transactions from the transaction pool
+    for _tx in transactionPool:
+        # Loop through the entries of each transaction
+        for tx_in in _tx.tx_ins:
+            # Check if input exists in unspent outputs
+            if not has_tx_in(tx_in, unspent_tx_outs):
+                # Add the transaction to the list of transactions
+                # invalid
+                invalid_txs.append(_tx)
+                # Break out of inner loop
+                break
+    # Check if list of invalid transactions is not empty
+    if len(invalid_txs) > 0:
+        # Display a message with invalid transactions at
+        # remove from pool
+        print(f"removing the following transactions from txPool: {invalid_txs}")
+        # Remove invalid transactions from transaction pool
+        transactionPool = [tx for tx in transactionPool if tx not in invalid_txs]
